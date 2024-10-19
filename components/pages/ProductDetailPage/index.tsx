b1a4a416abc6e6403594e6ef242cceb7d1f1c2e8
tonyandy5630 <tonyandy456@gmail.com>
2024-10-19 20:26:24 +0700

    <mod> move products page to app dir

--- components/pages/ProductDetailPage/index.tsx
+++ components/pages/ProductDetailPage/index.tsx
@@ -64,7 +64,7 @@ export default function ProductDetailPage({ productId }: Props) {
               </div>
               <div className='flex items-center gap-3'>
                 Stock:
-                <span className='font-bold'>{product.quantity}</span>
+                <span className='font-bold text-xl'>{product.quantity}</span>
               </div>
               <div>
                 <Button disabled>Add to Cart</Button>
